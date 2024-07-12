991481551411851af3c7bdb886bbd97608591a49
izzyn <isak.attila@gmail.com>
2024-07-12 20:32:15 +0200

    Horrible bag randomisation :)

--- .config/hypr/configs/swww.sh
+++ .config/hypr/configs/swww.sh
@@ -1,7 +1,12 @@
 swww-daemon &
 cd ~/.config/hypr/configs/backgrounds/.
 while : ; do
-   file="$(ls | sort -R | tail -1)"
+    if [ $(cat ../latestpapers.txt | wc -l) == 0 ]; then
+        ls >> ../latestpapers.txt
+        echo "what"
+    fi 
+   file="$(cat ../latestpapers.txt | sort -R | tail -1)"
+   sed -i "\#$file#d" ../latestpapers.txt
    swww img "$file" --transition-type fade
    sleep 300
 done
