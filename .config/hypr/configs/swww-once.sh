43bb2311825b991df07f96385c7dd58d83812992
izzyn <isak.attila@gmail.com>
2024-07-12 20:32:15 +0200

    Horrible bag randomisation :)

--- .config/hypr/configs/swww-once.sh
+++ .config/hypr/configs/swww-once.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 cd ~/.config/hypr/configs/backgrounds/.
+
+if [ $(cat ../latestpapers.txt | wc -l) == 0 ]; then
+    ls >> ../latestpapers.txt
+    echo "what"
+fi 
+
 killall mpvpaper
-file="$(ls | sort -R | tail -1)"
+file="$(cat ../latestpapers.txt | sort -R | tail -1)"
+sed -i "\#$file#d" ../latestpapers.txt
+
 swww img "$file" --transition-type fade
