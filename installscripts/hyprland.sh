44bf515ceb1015763c81935626a1c9b0c0600500
Spizzy1 <isak.attila@gmail.com>
2024-07-26 22:47:55 +0200

    More stuff :)

--- installscripts/hyprland.sh
+++ installscripts/hyprland.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-yay -S wl-clipboard hyprland swww rofi-wayland waybar pywal hyprcursor hyprlock hypridle hyprshot
+yay -S wl-clipboard mpvpaper hyprland swww rofi-wayland waybar pywal hyprcursor hyprlock hypridle hyprshot
---
+++ sddm-themes/setup.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+systemctl enable sddm.service
+
+sudo cp -r simple-login-sddm-theme/ /usr/share/sddm/simple-login
+
+echo "Edit the [Theme] /usr/sddm/sddm.conf.d/default.conf section 'Current' to 'simple-login'"
+
+echo "AFTER FILLING IN THIS YOUR COMPUTER WILL RESTART"
+./simple-login-sddm-theme/vagrant_setup.sh
