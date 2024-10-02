01669af32631c844a29966c35d30fa5b0c2edff6
Spizzy1 <isak.attila@gmail.com>
2024-10-03 00:37:47 +0200

    Automatic formatting :)))

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,3 +24,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.wo.relativenumber = true
 vim.cmd[[colorscheme dracula]]
+vim.api.nvim_create_augroup('AutoFormatting', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.rs',
+  group = 'AutoFormatting',
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
+})
