50a86659591f832791d825c7cd620066a89f8f04
Spizzy1 <isak.attila@gmail.com>
2025-04-06 18:59:12 +0200

    no formatting

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,15 +1,5 @@
 local lsps = {"angularls", "rust_analyzer","lua_ls", "pyright", "csharp_ls", "clangd", "glsl_analyzer", "arduino_language_server"}
 
-local on_attach = function(client, bufnr)
-  -- format on save
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("Format", { clear = true }),
-      buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
-    })
-  end
-end
 return{
 {
         "williamboman/mason.nvim",
@@ -32,7 +22,6 @@ return{
         lspconfig.csharp_ls.setup({})
         lspconfig.cssls.setup({})
         lspconfig.tsserver.setup({
-        on_attach = on_attach,
         filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
         cmd = { "typescript-language-server", "--stdio" }})
         lspconfig.glsl_analyzer.setup({})
